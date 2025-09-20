43d41fb11433b29c3b0093b17c1a5e66dc490742
Victor <Victorfoliveira0407@gmail.com>
2025-09-20 15:02:03 -0300

    Criação do POST e do DELETE

--- src/controllers/batfamilyController.js
+++ src/controllers/batfamilyController.js
@@ -26,4 +26,73 @@ const batfamilyGetById = (req, res) => {
     }
 }
 
-export {batfamilyGetAll, batfamilyGetById}
+const createMember = (req, res) => {
+    const {codinome, nomeVerdadeiro, habilidade} = req.body;
+
+    if (!codinome) {
+        res.status(400).json({
+            "succes": false,
+            "message": "Codinome é obrigatório!"
+        });
+    }
+
+    if (!nomeVerdadeiro) {
+        res.status(400).json({
+            "succes": false,
+            "message": "Nome verdadeiro é obrigatório!"
+        });
+    }
+
+    if (!habilidade) {
+        res.status(400).json({
+            "succes": false,
+            "message": "Habilidade é obrigatório!"
+        });
+    }
+
+    const newMember = {
+        id: batfamily.length + 1,
+        nomeVerdadeiro,
+        habilidade
+    }
+
+    batfamily.push(newMember);
+
+    res.status(201).json({
+        "succes": true,
+        "message": "Novo membro da batfamilia cadastrado com sucesso",
+        "data": newMember
+    });
+}
+
+const deleteMember = (req, res) => {
+    const {id} = req.params;
+
+    if (isNaN(id)) {
+        res.status(400).json({
+            "succes": false,
+            "message": "O ID deve ser válido"
+        });
+    }
+
+    const idParaApagar = parseInt(id);
+    const membroParaRemover = batfamily.find((m) => m.id === idParaApagar);
+
+    if (!membroParaRemover) {
+        res.status(200).json({
+            "succes": false,
+            "message": `O ID ${id} não encontrado`
+        });
+    }
+
+    const membroFiltrado = batfamily.filter((m) => m.id !== id);
+
+    batfamily.splice(0, batfamily.length, ...membroFiltrado);
+
+    return res.status(200).json({
+        "succes": true,
+        "message": "O membro da batfamília foi removido com sucesso",
+    });
+}
+
+export {batfamilyGetAll, batfamilyGetById, createMember, deleteMember}
--- src/routes/batfamilyRoutes.js
+++ src/routes/batfamilyRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import {batfamilyGetAll} from "../controllers/batfamilyController.js";
 import {batfamilyGetById} from "../controllers/batfamilyController.js";
+import {createMember} from "../controllers/batfamilyController.js";
+import {deleteMember} from "../controllers/batfamilyController.js";
 
 const router = express.Router();
 
 router.get("/", batfamilyGetAll);
 router.get("/:id", batfamilyGetById);
+router.post("/", createMember);
+router.delete("/:id", deleteMember);
 
 export default router;
